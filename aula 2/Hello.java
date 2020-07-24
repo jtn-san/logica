530b5e7ef13ff64458242bcf1465ada651c2cdc6
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-07-24 13:42:05 -0300

    Hello World

---
+++ aula 2/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author Jonathan
+ * Hello World
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
