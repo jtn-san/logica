05cc8fdcf49f3bc0023df41cee29b584a8a9caae
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-07-24 13:56:23 -0300

    Curso Java

---
+++ aula 2/Fundamentos.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fundamentos;
+
+/**
+ * @author Jonathan
+ *
+ */
+public class Fundamentos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String nome = "Jonathan";
+		int idade = 27;
+		char sexo = 'M';
+		double temperatura = 26.7;
+		boolean arCondicionado = false;
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Sexo: " + sexo);
+		System.out.println("Temperatura: " + temperatura);
+		System.out.println("Ar Condicionado: " + arCondicionado);
+
+	}
+
+}
