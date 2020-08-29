81d7c0a46369b425863b3814211da3bd6f3d3305
Jonathan Santos <57300906+jtn-san@users.noreply.github.com>
2020-08-28 21:41:17 -0300

    Calcular IMC
    Índice de massa corporal

--- aula 4/CalcularImc.java
+++ aula 4/CalcularImc.java
@@ -19,7 +19,7 @@ public class CalcularImc {
 		double altura,peso,seuimc;
 		Scanner teclado = new Scanner(System.in);
 		DecimalFormat formatador = new DecimalFormat("#0.00");
-		System.out.println("Cálculo do IMC");
+		System.out.println("CÃ¡lculo do IMC");
 		System.out.print("Digite a altura (ex. 1,60): ");
 		altura = teclado.nextDouble(); 
 		System.out.print("Digite a peso (ex. 60): ");		
@@ -29,7 +29,7 @@ public class CalcularImc {
 		System.out.print("Seu IMC: " + formatador.format(seuimc));
 		if (seuimc < 18.5) {
 			System.out.println(" ABAIXO DO PESO");
-		} else if (seuimc >= 18.6 && seuimc < 24.9) {
+		} else if (seuimc >= 18.5 && seuimc < 24.9) {
 			System.out.println(" PESO NORMAL");
 		} else if (seuimc >= 25.0 && seuimc < 29.9) {
 			System.out.println(" ACIMA DO PESO");
